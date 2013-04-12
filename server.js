f77de05f30579476dbd85b7d129c508ec4d29737
Russell Smith <russell.smith@ukd1.co.uk>
2013-04-12 10:29:22 -0700

    Remove redis, add vows

--- server.js
+++ server.js
@@ -103,38 +103,6 @@
     });
   };
 
-  /* REDIZZZZZ
-  */
-
-
-  if (process.env.REDISTOGO_URL) {
-    rtg = require("url").parse(process.env.REDISTOGO_URL);
-    redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
-  } else {
-    redis = require("redis").createClient();
-  }
-
-  if (global.data == null) {
-    global.data = {};
-  }
-
-  global.redis = redis;
-
-  global.redis.on("error", function(err) {
-    return console.log("Error " + err);
-  });
-
-  global.redis.on("connect", function() {
-    global.redis.incr('started');
-    global.redis.get('started', function(err, response) {
-      if (!err) {
-        return console.log('Started', response, 'times');
-      }
-    });
-    return main();
-  });
-
   app.listen(process.env.PORT || 5000);
 
 }).call(this);
